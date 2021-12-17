e80ed472784e7ac3a08f8bb29f6263988f594804
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2021-12-17 10:34:54 +0200

    gmail send mail class

--- gmail_send_mail.py
+++ gmail_send_mail.py
@@ -1,3 +1,4 @@
+import os
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
@@ -28,3 +29,73 @@ def send_mail(text='Python body', subject='Python subject', from_email=f'Python 
 
 
 send_mail(subject='subject here', text='email body text here',  to_emails=['..........@gmail.com'])
+
+
+
+class Emailer:
+  host = 'smtp.gmail.com'
+  port = 587
+  username = '' # email
+  password = '' # password
+  from_email = f'Python Mail<{username}>'
+  subject = ''
+  text = None
+  context = {}
+  to_emails = []
+  test_send = False
+  has_html = False
+  template_html = None
+  template_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+
+
+  def __init__(self, subject='', text=None, context={}, to_emails=None, template_html=None, test_send=False, *args, **kwargs):
+    assert isinstance(to_emails, list)
+    self.subject = subject
+    self.context = context
+    self.to_emails = to_emails
+    self.test_send = test_send
+    self.text = text
+    if template_html != None:
+      self.template_html = template_html
+      self.has_html = True
+
+  def format_msg(self):
+    msg = MIMEMultipart('alternative')
+    msg['From'] = self.from_email
+    msg['To'] = ', '.join(self.to_emails)
+    msg['Subject'] = self.subject
+    if self.text != None:
+      txt_part = MIMEText(self.text, 'plain')
+      msg.attach(txt_part)
+    if self.template_html:
+      self.template_path = os.path.join(self.template_path, self.template_html)
+      if not os.path.exists(self.template_path):
+        raise Exception('This path does not exist')
+      template_string = ''
+      with open(self.template_path, 'r') as f:
+        template_string = f.read()
+        template_string = template_string.format(**self.context)
+    if self.template_html != None:
+      html_part = MIMEText(template_string, 'html')
+      msg.attach(html_part)
+    msg_str = msg.as_string()
+    return msg_str
+
+  def send(self):
+    msg = self.format_msg()
+    did_send = False
+    if not self.test_send:
+      with smtplib.SMTP(host=self.host, port=self.port) as server:
+        server.ehlo()
+        server.starttls()
+        server.login(self.username, self.password)
+        try:
+          server.sendmail(self.from_email, self.to_emails, msg)
+          did_send = True
+        except:
+          did_send = False
+    return did_send
+
+
+obj = Emailer(subject='subject here', text='email body text here', template_html='template.html', context={'name': 'OSAMA'},  to_emails=['..........@gmail.com'],  test_send=False)
+obj.send()

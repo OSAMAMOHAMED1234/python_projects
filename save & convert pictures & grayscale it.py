33936fb9a38e7f92f4f21df12372ded698ac79fa
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2022-02-05 16:17:33 +0200

    delete unneeded file

--- save & convert pictures & grayscale it.py
+++
@@ -1,4 +0,0 @@
-from PIL import Image
-img = Image.open(r"C:\Users\OSAMA\Desktop\hijab.png").convert('L') # pic distnation
-img.show() # open pic
-img.save('new_pic_name.png') # save pic as a new type & new name

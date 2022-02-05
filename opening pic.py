ef1b862d12ae6c281434d62a96632ca6ad7b65d9
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2022-02-05 15:48:27 +0200

    dynamic save images

--- opening pic.py
+++ opening pic.py
@@ -1,18 +1,13 @@
 import os
 from PIL import Image
 
-img = Image.open(r"C:\Users\OSAMA\Desktop\hijab.png").show()
-##img.show()
-os.startfile(r"C:\Users\OSAMA\Desktop\hijab.png")
+img = Image.open(os.path.join(os.path.dirname(__file__), '1.png')).show()
+os.startfile(os.path.join(os.path.dirname(__file__), '1.png'))
 
-from PIL import Image
-img = Image.open(r"C:\Users\OSAMA\Desktop\hijab.png").convert('L')
+img = Image.open(os.path.join(os.path.dirname(__file__), '1.png')).convert('L')
 img.show()
-img.save('newh.jpg')
+img.save('2.jpg')
 
-
-from PIL import Image
-img = Image.open(r"C:\Users\OSAMA\Desktop\hijab.png")#.convert('L')
+img = Image.open(os.path.join(os.path.dirname(__file__), '1.png'))#.convert('L')
 new_img = img.resize((256,256))
-new_img.save('brick-house-256x256.png','png')
-
+new_img.save('2-256x256.png', 'png')

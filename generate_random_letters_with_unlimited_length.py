7324a6113b66f4230b0c255e71509d041b64253a
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2022-02-05 15:31:42 +0200

    add punctuate

--- generate_random_letters_with_unlimited_length.py
+++ generate_random_letters_with_unlimited_length.py
@@ -1,3 +1,6 @@
-import random
-import string
-print("".join([random.choice(string.ascii_letters + string.digits) for i in range(250)]))
+import random, string
+
+def random_token(n):
+  return ''.join([random.choice(string.ascii_letters + string.digits + string.punctuation) for _ in range(n)])
+
+print(random_token(250))

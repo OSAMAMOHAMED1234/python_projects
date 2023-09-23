015c52b757c24c4cf57f6a567309aa6b1a542758
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2023-09-23 17:11:24 +0200

    set symmetric difference

--- set/symmetric_difference.py
+++ set/symmetric_difference.py
@@ -1,3 +1,13 @@
 a = set(map(int, input().split()))
 b = set(map(int, input().split()))
 print(len(a.symmetric_difference(b))) # ^
+
+
+
+# a = set(map(int, input().split()))
+# b = set(map(int, input().split()))
+# set_a_diff = a.difference(b)
+# set_b_diff = b.difference(a)
+# new_set = sorted(set_a_diff.union(set_b_diff))
+# for i in new_set:
+#   print(i)

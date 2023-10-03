d72fd81a3973f210e27b5b358e0e4098a6de913d
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2023-10-03 18:38:28 +0300

    update square print

--- funny print/solid_square.py
+++
@@ -1,5 +0,0 @@
-rows = 6
-for i in range(rows):
-  for j in range(rows):
-    print('*', end='')
-  print()
--- funny print/square.py
+++ funny print/square.py
@@ -1,4 +1,13 @@
 rows = 6
+# solid
+for i in range(rows):
+  for j in range(rows):
+    print('*', end='')
+  print()
+
+print('*' * 50)
+
+# hollow
 for i in range(rows):
   for j in range(rows):
     if i == 0 or i == rows - 1 or j == 0 or j == rows - 1:

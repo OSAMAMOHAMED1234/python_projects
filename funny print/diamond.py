7ccf40fda1d030ec72c369fbcb1c73e5d7cbf7a0
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2023-10-05 11:12:04 +0300

    update diamond

--- funny print/diamond.py
+++ funny print/diamond.py
@@ -1,14 +1,20 @@
 rows = 10
-for i in range(1, rows + 1):
-  for j in range(1, rows - i + 1):
-    print(' ', end='')
-  for k in range(1, 2 * i):
-    print('*', end='')
-  print()
+for row in range(rows):
+  print(' ' * (rows - row - 1) + '*' * (2 * row + 1))
+for row in range(rows - 2, -1, -1):
+  print(' ' * (rows - row - 1) + '*' * (2 * row + 1))
 
-for i in range(rows - 1, 0, -1):
-  for j in range(1, rows - i + 1):
-    print(' ', end='')
-  for k in range(1, 2 * i):
-    print('*', end='')
-  print()
+
+# for row in range(rows):
+#   for _ in range(rows - row - 1):
+#     print(' ', end='')
+#   for _ in range(2 * row + 1):
+#     print('*', end='')
+#   print()
+
+# for row in range(rows - 2, -1, -1):
+#   for _ in range(rows - row - 1):
+#     print(' ', end='')
+#   for _ in range(2 * row + 1):
+#     print('*', end='')
+#   print()

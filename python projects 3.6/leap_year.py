a7937478c77d0d84ea9a6018a81279a2579e140d
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2023-10-11 11:40:52 +0300

    update leap year

--- python projects 3.6/leap_year.py
+++ python projects 3.6/leap_year.py
@@ -1,8 +1,17 @@
-def check_leap(year):  
-  if((year % 400 == 0) or (year % 100 != 0) and (year % 4 == 0)):      
-    return f"{year} is a leap Year"
-  else:  
-    return f"{year} is not a leap Year"
+def leap_years(start_year, end_year):
+  leap_years = []
+  for year in range(start_year, end_year + 1):
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+      leap_years.append(year)
+  return leap_years
+print(leap_years(2000, 2030))
 
-for x in range(1990, 2050):
-  print(check_leap(x))
+
+def is_leap_year(year):
+  return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
+
+def print_leap_years(start_year, end_year):
+  for year in range(start_year, end_year + 1):
+    if is_leap_year(year):
+      print(year)
+print_leap_years(2000, 2030)

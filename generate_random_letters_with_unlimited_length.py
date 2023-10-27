c5631cb0c94489014437fdbe5928b331aba7a650
OSAMA MOHAMED <33071700+osama-mohamed@users.noreply.github.com>
2023-10-27 18:53:02 +0200

    Update generate_random_letters_with_unlimited_length.py

--- generate_random_letters_with_unlimited_length.py
+++ generate_random_letters_with_unlimited_length.py
@@ -1,8 +1,25 @@
 import random, string
 
 def random_token(n):
+  # limited_punctuation = random.sample(string.punctuation, 3)  # Gets 3 random punctuation marks
+  limited_punctuation = '!@#$%^&*'
+  choices = string.ascii_letters + string.digits + ''.join(limited_punctuation)
+  return ''.join([random.choice(choices) for _ in range(n)])
+print(random_token(int(50)))
+
+def random_token(n):
   return ''.join([random.choice(string.ascii_letters + string.digits + string.punctuation) for _ in range(n)])
 print(random_token(250))
+
+
+def random_token(n):
+  max_num_of_punc = 3
+  limited_punctuation = '!@#$%^&*'
+  token_part1 = ''.join([random.choice(string.ascii_letters + string.digits) for _ in range(n - max_num_of_punc)])
+  token_part2 = ''.join([random.choice(limited_punctuation) for _ in range(max_num_of_punc)])
+  choices = token_part1 + token_part2
+  return ''.join(random.sample(choices, len(choices)))
+print(random_token(50))
 
 
 class RandomSerialGenerator:
